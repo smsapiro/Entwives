e0bc27de983edae0b7f0361835226cc9bef4e865
Solomon Spairo <smsapiro@gmail.com>
2020-01-14 22:36:24 +0000

    Initial commit

---
+++ quest.js
@@ -0,0 +1,32 @@
+var questTree = {
+    text: "Well hello there little Entling! The Entwives are lost. " +
+        "You have to go find them! Which way will you search?",
+    options: [
+        {
+            text: "North",
+            outcome: function () {
+                return {
+                    text: "Arrive at Lorien.",
+                    options: [
+                        {
+                            text: "Do nothing",
+                            outcome: null
+                        }
+                    ]
+                }
+            }
+        },
+        {
+            text: "East",
+            outcome: null
+        },
+        {
+            text: "South",
+            outcome: null
+        },
+        {
+            text: "West",
+            outcome: null
+        },
+    ]
+};
