792453059502e8357cc6254266dbcea077a64298
Solomon Spairo <smsapiro@gmail.com>
2020-01-16 18:52:40 +0000

    file structure

--- quest.js
+++ quest.js
@@ -1,7 +1,7 @@
 function quest(playerName) {
     var questTree = {
         text: "Well hello there " + playerName + " my fellow Ent! What’s that you say, you are going away to search for the Entwives!? Young and brave you are my friend, and quite an adventure that will be. I’ll help you how I may, but the Entwives have not been seen for many ages. Oh how I miss them so. To where shall you first set off?",
-        img: "treebeard.png",
+        img: "images/treebeard.png",
         options: [{
                 text: "North",
                 outcome: function () {
