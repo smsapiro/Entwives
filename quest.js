ad47f7f0776b3570e5c1ab2b423d6c8002ec91c6
Solomon Spairo <smsapiro@gmail.com>
2020-01-16 16:40:20 +0000

    Treebeard monologue and improved resizing

--- quest.js
+++ quest.js
@@ -1,6 +1,6 @@
 var questTree = {
-    text: "Well, hello there! The Entwives are lost. " +
-        "You have to go find them! Which way will you search?",
+    text: "Well hello there my fellow Ent! What’s that you say, you are going away to search for the Entwives!? Young and brave you are my friend, and quite an adventure that will be. I’ll help you how I may, but the Entwives have not been seen for many ages. Oh how I miss them so. To where shall you first set off?",
+    img: "treebeard.png",
     options: [
         {
             text: "North",
