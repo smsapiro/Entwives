82c97aa951d3f2eb810aa5b4558284b864dfd6d8
Solomon Spairo <smsapiro@gmail.com>
2020-01-16 18:44:30 +0000

    Name selection

--- quest.js
+++ quest.js
@@ -1,32 +1,32 @@
-var questTree = {
-    text: "Well hello there my fellow Ent! What’s that you say, you are going away to search for the Entwives!? Young and brave you are my friend, and quite an adventure that will be. I’ll help you how I may, but the Entwives have not been seen for many ages. Oh how I miss them so. To where shall you first set off?",
-    img: "treebeard.png",
-    options: [
-        {
-            text: "North",
-            outcome: function () {
-                return {
-                    text: "Arrive at Lorien.",
-                    options: [
-                        {
+function quest(playerName) {
+    var questTree = {
+        text: "Well hello there " + playerName + " my fellow Ent! What’s that you say, you are going away to search for the Entwives!? Young and brave you are my friend, and quite an adventure that will be. I’ll help you how I may, but the Entwives have not been seen for many ages. Oh how I miss them so. To where shall you first set off?",
+        img: "treebeard.png",
+        options: [{
+                text: "North",
+                outcome: function () {
+                    return {
+                        text: "Arrive at Lorien.",
+                        options: [{
                             text: "Do nothing",
                             outcome: null
-                        }
-                    ]
+                        }]
+                    }
                 }
-            }
-        },
-        {
-            text: "East",
-            outcome: null
-        },
-        {
-            text: "South",
-            outcome: null
-        },
-        {
-            text: "West",
-            outcome: null
-        },
-    ]
-};
+            },
+            {
+                text: "East",
+                outcome: null
+            },
+            {
+                text: "South",
+                outcome: null
+            },
+            {
+                text: "West",
+                outcome: null
+            },
+        ]
+    };
+    return questTree;
+}
